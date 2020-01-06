f8b37382c448da9fa0d607d8db00284cca1ac68e
Mohsen Hosseini <mohsen.hosseini@neocard.fi>
2020-01-06 18:17:06 +0200

    added database migrate

---
+++ db/migrations/000001_create_event_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `description` text,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
